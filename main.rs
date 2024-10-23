a03405bb77eff64d514887be8df4c7a1f737eda7
Zane <z.pearton@accenture.com>
2024-10-23 22:15:27 +1100

    devto - serve

---
+++ main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("This project contains multiple binaries.");
+    println!("To run the server: cargo run --bin server");
+    println!("To run the client: cargo run --bin metrics_client");
+}
