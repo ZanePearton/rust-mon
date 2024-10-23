4fc52ccdeaa9027d65b72656d77c799bbbd4df9d
Zane <z.pearton@accenture.com>
2024-10-23 22:18:06 +1100

    remove duplicates

--- wasi_metrics/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("This project contains multiple binaries.");
-    println!("To run the server: cargo run --bin server");
-    println!("To run the client: cargo run --bin metrics_client");
-}
